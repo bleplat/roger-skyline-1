8c47cbec15446d3821ded78ab827229e29d0f7cb
user <user@127.0.0.1>
2020-05-20 20:53:07 +0200

    fixed update output

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo mkdir -p /var/log/
+sudo mkdir -p /var/log/ > /dev/null
 
-sudo apt update >> /var/log/update_script.log
-sudo apt --yes upgrade >> /var/log/update_script.log
+sudo apt update >> /var/log/update_script.log 2>&1
+sudo apt --yes upgrade >> /var/log/update_script.log 2>&1
 
