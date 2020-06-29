857e2f060069ba3b6ec3234883da56789647a124
user <user@127.0.0.1>
2020-06-29 20:05:10 +0200

    ed

--- disable_services.sh
+++ disable_services.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env bash
 
-SERVICES=$(systemctl list-unit-files | grep enabled | sed "s/^\(.*\)enabled.*$/\1/" | sed "s/[[:space:]]*$//")
 REQUIERED="autovt@.service getty@.service networking.service ssh.service sshd.service syslog.service rsyslog.service cron.service fail2ban.service"
+
+
+# Disable kind services
+
+SERVICES=$(systemctl list-unit-files | grep enabled | sed "s/^\(.*\)enabled.*$/\1/" | sed "s/[[:space:]]*$//")
 
 echo "$SERVICES" | while IFS= read -r line ; do
 	if [[ $REQUIERED == *"$line"* ]]; then
 		echo "keeping $line enabled"
 	else
 		echo "disabling $line"
-		sudo systemctl disable "$line"
+		sudo systemctl --now disable "$line"
 	fi
 done
+
+
+# That's what you got
+
+SERVICES=$(systemctl list-unit-files | grep enabled | sed "s/^\(.*\)enabled.*$/\1/" | sed "s/[[:space:]]*$//")
+
+echo "$SERVICES" | while IFS= read -r line ; do
+	if [[ $REQUIERED == *"$line"* ]]; then
+		echo "keeping $line enabled"
+	else
+		echo "disabling $line"
+		sudo systemctl --now mask "$line"
+	fi
+done
+
+
+
+
